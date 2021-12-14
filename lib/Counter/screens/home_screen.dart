16822d4fc5f2d29c957fdf4bf489515721a43fac
membri92 <membri_2@hotmail.com>
2021-12-14 19:50:26 +0100

    finishing counter

--- lib/Counter/screens/home_screen.dart
+++ lib/Counter/screens/home_screen.dart
@@ -3,43 +3,31 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-
-    const fontSize30 =  TextStyle( fontSize: 30);
+    const fontSize30 = TextStyle(fontSize: 30);
     int counter = 0;
 
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Homescreen'),
         centerTitle: true,
         elevation: 10.0,
       ),
       body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-            children:  [
-              const Text('Clicks Number', style: fontSize30 ),
-            Text('$counter', style: fontSize30),
-
-
-            ]),
-        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const Text('Clicks Number', style: fontSize30),
+          Text('$counter', style: fontSize30),
+        ]),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add, size: 42.0),
         onPressed: () {
-
           counter++;
           print('Me has pulsado: $counter');
         },
-        
-      ) ,
+      ),
     );
-
-
   }
-
 }
