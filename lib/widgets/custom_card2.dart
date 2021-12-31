7cf3c493bbeba0fe1c8db05507feb34d2b71d9ad
membri92 <membri_2@hotmail.com>
2021-12-31 16:15:42 +0100

    setting image properties

--- lib/widgets/custom_card2.dart
+++ lib/widgets/custom_card2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning/themes/app_themes.dart';
 
 
 
@@ -9,13 +10,28 @@ class CustomCard2 extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15)
+      ),
       elevation: 10,
+      shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
-        children:  const [
+        children:    [
 
-          FadeInImage(
+        const  FadeInImage(
               image: NetworkImage('https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/130238819/original/d4096d4950eba421600f21c6c753c19375222eb6/draw-you-a-landscape-image-with-ms-paint.png'),
             placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: const Text('A Landscape'),
           )
         ],
       ),
